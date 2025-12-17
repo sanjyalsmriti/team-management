20cfba7617e57cf048b79b9432b68833a08b759f
sanjyalsmriti <sanjyalsmriti@gmail.com>
2025-12-17 13:03:02 +0545

    class name is added for UserCard

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,16 +1,21 @@
-import  { memo } from 'react';
+import React, { memo } from 'react';
+import '../styles/UserCard.css';
 
+const UserCard = memo(({ user, onClick }) => {
+  const handleClick = () => onClick(user);
+  
+  return (
+    <div className="user-card" onClick={handleClick}>
+      <div className="user-card-content">
+        <h3 className="user-name">{user.name}</h3>
+        <p className="user-email">{user.email}</p>
+        <p className="user-company">{user.company?.name || 'N/A'}</p>
+      </div>
+    </div>
+  );
+});
 
-const UserCard = memo (({user,onClick })=>{
-    const handleClick = () => onClick(user);
-    return(
-        <div onClick={handleClick}>
-            <div>
-                <h3>{user.name}</h3>
-                <p>{user.email}</p>
-                <p>{user.company?.name || 'N/A'}</p>
-            </div>
-        </div>
-    )
-})
+UserCard.displayName = 'UserCard';
+
 export default UserCard;
+
