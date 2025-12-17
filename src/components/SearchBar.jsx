c7b7ab7a1686ec6ad158c548dfb3c586416b9d7b
sanjyalsmriti <sanjyalsmriti@gmail.com>
2025-12-17 13:01:33 +0545

    search bar component is added and removed from the user list and implemented there

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/SearchBar.css';
+
+const SearchBar = ({ searchTerm, onSearchChange }) => {
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        placeholder="Search users by name..."
+        value={searchTerm}
+        onChange={(e) => onSearchChange(e.target.value)}
+        className="search-input"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
+
+
