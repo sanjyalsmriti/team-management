c957997a946545001144dd36677b976f4cabed0c
sanjyalsmriti <sanjyalsmriti@gmail.com>
2025-12-17 13:06:26 +0545

    Users page customized and optimized

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,12 +1,47 @@
-import UserList from "../components/UserList";
-const Users = () =>{
-    return(
-       <div>
+import React, { memo } from 'react';
+import UserList from '../components/UserList';
+import UserDetailPanel from '../components/UserDetailPanel';
+import { useFetchUsers } from '../hooks/useFetchUsers';
+import { useSelector } from 'react-redux';
+import '../styles/Users.css';
+
+const Users = () => {
+  useFetchUsers();
+  const { loading, error } = useSelector((state) => state.users);
+
+  if (loading) {
+    return (
+      <div className="users-page">
+        <div className="loading-container">
+          <p>Loading users...</p>
+        </div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="users-page">
+        <div className="error-container">
+          <p className="error-message">Error: {error}</p>
+          <p>Please try refreshing the page.</p>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="users-page">
+      <div className="page-header">
         <h2>Team Members</h2>
         <p>Click on any user card to view detailed information</p>
-        <UserList/>
-</div>
-    )
+      </div>
+      <UserList />
+      <UserDetailPanel />
+    </div>
+  );
+};
 
-}
-export default Users;
+export default memo(Users);
+
+
