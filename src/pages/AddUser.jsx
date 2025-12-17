91594572cc8260f70c5b28c170ee1adf77bc5764
sanjyalsmriti <sanjyalsmriti@gmail.com>
2025-12-17 13:05:59 +0545

    added className and css for Add uSer component

--- src/pages/AddUser.jsx
+++ src/pages/AddUser.jsx
@@ -1,16 +1,19 @@
-import AddUserForm from "../components/AddUserForm"
+import React from 'react';
+import AddUserForm from '../components/AddUserForm';
+import '../styles/AddUser.css';
 
+const AddUser = () => {
+  return (
+    <div className="add-user-page">
+      <div className="page-header">
+        <h2>Add New Team Member</h2>
+        <p>Fill out the form below to add a new user to your team</p>
+      </div>
+      <AddUserForm />
+    </div>
+  );
+};
 
-
-
-const AddUser = () =>{
-    return(
-        <div>
-            <div>
-                <p>AddUser Here</p>
-            </div>
-            <AddUserForm />
-        </div>
-    )
-}
 export default AddUser;
+
+
