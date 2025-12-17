1763db34fc78879941fc1cf4359949be6d3f3595
sanjyalsmriti <sanjyalsmriti@gmail.com>
2025-12-17 12:43:10 +0545

    addUser added in pages

---
+++ src/pages/AddUser.jsx
@@ -0,0 +1,16 @@
+import AddUserForm from "../components/AddUserForm"
+
+
+
+
+const AddUser = () =>{
+    return(
+        <div>
+            <div>
+                <p>AddUser Here</p>
+            </div>
+            <AddUserForm />
+        </div>
+    )
+}
+export default AddUser;
